4800765455ba96eb6e6533f70722a3a8a4b6cde7
formacion091-maker <formacion091@gmail.com>
2026-05-08 21:53:15 -0500

    nuevo2

--- database.sql
+++ database.sql
@@ -6,5 +6,6 @@ CREATE TABLE envios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     destinatario VARCHAR(100) NOT NULL,
     direccion VARCHAR(200) NOT NULL,
-    descripcion TEXT NOT NULL
+    descripcion TEXT NOT NULL,
+    telefono VARCHAR(20) NOT NULL
 );
