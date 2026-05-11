629bea66e85ba627fb5cecb381a167500666b027
danielcardonang1012-sketch <danielcardona.ng1012@gmail.com>
2026-05-11 11:12:42 -0500

    daniel1.1.2

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "mysql-edwinagudelo.alwaysdata.net";
-$usuario = "edwinagudelo";
+$host = "mysql-cardonalan.alwaysdata.net";
+$usuario = "cardonalan";
 $password = "clase1234";
-$bd = "edwinagudelo_mensajeria";
+$bd = "cardonalan_mensajeria";
 
 $conn = new mysqli($host, $usuario, $password, $bd);
 
